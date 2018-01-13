bf32cf8e34e8ac8a1a065984df1e1efbc8ce3e9b
d34dfr4m3 <32407502+d34dfr4m3@users.noreply.github.com>
2018-01-13 19:47:24 -0200

    Update and rename another.sh to handler.sh

--- handler.sh
+++ handler.sh
@@ -14,14 +14,15 @@ else
 	while read -rs -n 1 c && [[ $c != 'q' ]]
 	do
 		if [ $(echo $c | wc -c )  -eq 0  ];then
-				echo "[!] Breaking"
 				break
 			elif [ $c != '\r' ];then
 				string=$string+$c
 			else
-				echo "$( echo $string | tr -d '+')"
+				move = $( echo $string | tr -d '+')
+				send $move & 
+				echo $move
 				string=''
-				send & 
+				
 		fi
 
 	done < $1
