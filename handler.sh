42f9efbfa8d7f0ccaed1a23e441026c7bd032a85
d34dfr4m3 <32407502+d34dfr4m3@users.noreply.github.com>
2018-01-15 20:52:55 -0200

    Webcam image capture added

--- handler.sh
+++ handler.sh
@@ -1,29 +1,22 @@
 #!/bin/bash
 function send(){
-
-	echo "Intruder Detected at $(date) -> Distance: $1" | mutt -s "Root Intruder Detected " <your_email@dotcom.hue> 
-
+	streamer -t 10 -r 2 -o images/target00.jpeg
+	echo "Intruder Detected at $(date) | Distance: $1" | mutt -s "Root Intruder Detected" -F .muttrc -a images/*.jpeg -- < EMAIL HERE>
 }
-if [ $# -lt 1 ] 
-	echo "[*] - Usage: 
-			$0 /dev/ttyXXX"
-	exit 1
-else
-	stty -F $1 raw
-	stty -F $1 -echo
-	while read -rs -n 1 c && [[ $c != 'q' ]]
-	do
-		if [ $(echo $c | wc -c )  -eq 0  ];then
-				break
-			elif [ $c != '\r' ];then
-				string=$string+$c
-			else
-				move = $( echo $string | tr -d '+')
-				send $move & 
-				echo $move
-				string=''
-				
-		fi
+stty -F $1 raw
+stty -F $1 -echo
+while read -rs -n 1 c && [[ $c != 'q' ]]
+do
+        if [ $(echo $c | wc -c )  -eq 0  ];then
+                        echo "[!] Breaking"
+                        break
+                elif [ $c != '\r' ];then
+                        string=$string+$c
+                else
+                        move=$( echo $string | tr -d '+')
+                        send $move &
+			echo $move
+                        string=''
+                fi
 
-	done < $1
-fi
+        done < $1
